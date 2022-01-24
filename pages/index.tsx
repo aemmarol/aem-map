82d5377699fbc71faa69f835f933f743467e2f27
adnandedhawala <adnan.d@somaiya.edu>
2022-01-25 00:53:45 +0530

    added sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Button } from 'antd';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.less';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
