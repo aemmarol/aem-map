90b7ad9697969a2fe80d88202525ad06d0134579
adnandedhawala <adnan.d@somaiya.edu>
2022-01-31 21:56:30 +0530

    added firebase login with custom api

--- pages/api/v1/authentication.ts
+++ pages/api/v1/authentication.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from "next";
 import Joi from "joi";
+import {getAuth, signInWithCustomToken} from "firebase/auth";
+import "../../../firebase/firebaseConfig";
 
 interface Data {
   name?: string;
@@ -18,14 +20,43 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === "POST") {
-    // console.log(loginSchema.validate(req.body));
-    const {error, value} = loginSchema.validate(req.body);
+    const auth = getAuth();
+
+    const {error} = loginSchema.validate(req.body);
+    const {itsId, password} = req.body;
+
     if (error) {
-      res.status(400).json({msg: error.details[0].message});
+      return res.status(400).json({msg: error.details[0].message});
     } else {
-      res.status(200).json({msg: "success", data: value});
+      await fetch(
+        process.env.NEXT_PUBLIC_AEM_AUTH_API_DOMAIN + "/api/authentication",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({itsId, password}),
+        }
+      )
+        .then((result) => {
+          if (result.ok) {
+            result.json().then((tokenData) => {
+              signInWithCustomToken(auth, tokenData.data).then((finalToken) => {
+                return res
+                  .status(200)
+                  .json({msg: "login Successful", data: finalToken});
+              });
+            });
+          } else {
+            return res.status(400).json({msg: "user not found"});
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.status(400).json({msg: "user not found"});
+        });
     }
   } else {
-    res.status(404).json({msg: "api not found"});
+    return res.status(404).json({msg: "api not found"});
   }
 }
