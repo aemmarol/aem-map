309330b4d0c798505dac1b3c85aea3a72bd081dd
alphanoobie <hussaingadi.hg@gmail.com>
2022-02-08 13:06:42 +0530

    sidebar user dropdown and nav options

--- layouts/dashboardLayout.tsx
+++ layouts/dashboardLayout.tsx
@@ -1,34 +1,63 @@
-import {Layout, Button, Menu, Drawer, Image, Divider} from "antd";
+import {Layout, Button, Menu, Drawer, Image, Divider, Dropdown} from "antd";
 import {FC} from "react";
 import styles from "../styles/layouts/DashboardLayout.module.scss";
 import {useState} from "react";
 const {Header, Content} = Layout;
-import {MenuOutlined} from "@ant-design/icons";
+import {DownOutlined, MenuOutlined, LogoutOutlined} from "@ant-design/icons";
 
 export const Dashboardlayout: FC = ({children}) => {
+  const handleNavClick = () => {
+    setVisible(false);
+  };
+
+  const userDropdownOverlay = (
+    <Menu>
+      <Menu.Item>
+        <LogoutOutlined className={styles.logoutlogo} />
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+
   const [visible, setVisible] = useState(false);
 
   return (
     <Layout>
       <Drawer
+        width={250}
         className={styles.sidebar}
         visible={visible}
         placement="left"
-        onClick={() => setVisible(false)}
         onClose={() => setVisible(false)}
       >
         <div className={styles.navhead}>
-          <Image src="/jamaatLogo.png" alt="logo" width={50} height={50} />
-          <span className={styles.navheadtitle}> AEM </span>
+          <Image
+            src="/jamaatLogo.png"
+            alt="logo"
+            width={100}
+            height={100}
+            preview={false}
+          />
         </div>
         <br />
-        <div className={styles.userinfo}>ITS ID</div>
+        <div className={styles.userinfo}>
+          <Dropdown overlay={userDropdownOverlay}>
+            <div>
+              ITS ID <DownOutlined />
+            </div>
+          </Dropdown>
+        </div>
         <Divider />
         <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-          <Menu.Item key="4">nav 4</Menu.Item>
+          <Menu.Item onClick={handleNavClick} key="1">
+            Map View
+          </Menu.Item>
+          <Menu.Item onClick={handleNavClick} key="2">
+            User List
+          </Menu.Item>
+          <Menu.Item onClick={handleNavClick} key="3">
+            Escalations
+          </Menu.Item>
         </Menu>
       </Drawer>
       <Layout>
