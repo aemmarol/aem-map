02816359588518289c47f8f7b6bfb68016b281d7
alphanoobie <hussaingadi.hg@gmail.com>
2022-02-10 21:23:01 +0530

    subsector list complete

--- pages/mohallah/subsector-list.tsx
+++ pages/mohallah/subsector-list.tsx
@@ -6,24 +6,49 @@ import {Dashboardlayout} from "../../layouts/dashboardLayout";
 import styles from "../../styles/SubSectorList.module.scss";
 
 const SubSectorList: NextPage = () => {
+  const sector = {
+    sector_name: "BADRI MOHALLAH",
+    sector_id: 1,
+    sub_sectors: [1, 2, 3, 4, 5],
+    color_code: "#A9D18E",
+    masool_name: "Mulla Yusuf Patanwala",
+    masool_its: 123456,
+    masool_contact_number: 1234567890,
+    masoola_name: "Munira Patanwala",
+    masoola_its: 654321,
+    masoola_contact_number: 987654321,
+  };
+
+  const subsector = {
+    sub_sector_name: "BURJ BURHAN",
+    sub_sector_id: 1,
+    sector_id: 1,
+    musaid_name: "Aziz Ahmedabadwala",
+    "musaid_ its": 123456,
+    musaid_contact_number: 1234567890,
+    musaida_name: "Farida Mandsaurwala",
+    musaida_its: 654321,
+    "musaida_contact number": 987654321,
+  };
+
   return (
-    <Dashboardlayout headerTitle="Badri Mohallah">
+    <Dashboardlayout headerTitle={sector.sector_name}>
       <Layout className={styles.mainWrapper}>
         <Row>
           <Col span={2}>
             <b>Masool:</b>
           </Col>
-          <Col span={3}>Mulla Yusuf Patanwala</Col>
-          <Col span={3}>123456</Col>
-          <Col span={3}>1234567890</Col>
+          <Col span={3}>{sector.masool_name}</Col>
+          <Col span={3}>{sector.masool_its}</Col>
+          <Col span={3}>{sector.masool_contact_number}</Col>
         </Row>
         <Row>
           <Col span={2}>
             <b>Masoola:</b>
           </Col>
-          <Col span={3}>Farida Patanwala</Col>
-          <Col span={3}>123456</Col>
-          <Col span={3}>1234567890</Col>
+          <Col span={3}>{sector.masoola_name}</Col>
+          <Col span={3}>{sector.masoola_its}</Col>
+          <Col span={3}>{sector.masoola_contact_number}</Col>
         </Row>
         <Row>
           <Col span={2}>
@@ -39,10 +64,10 @@ const SubSectorList: NextPage = () => {
               <h2 className={styles.cardHeading}>Burj Burhan</h2>
               <ul>
                 <li>
-                  <b>Musaid:</b> Aziz Ahmedabadwala
+                  <b>Musaid:</b> {subsector.musaid_name}
                 </li>
                 <li>
-                  <b>Musaida:</b> Farida Mandsaurwala
+                  <b>Musaida:</b> {subsector.musaid_name}
                 </li>
                 <li>
                   <b>Distance:</b> 0.25km/1 Minute
