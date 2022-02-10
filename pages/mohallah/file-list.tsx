856fd9650771d6a29d55c2e52463798cea20c0a9
adnandedhawala <adnan.d@somaiya.edu>
2022-02-10 21:53:58 +0530

    added temp pages

---
+++ pages/mohallah/file-list.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import {Dashboardlayout} from "../../layouts/dashboardLayout";
+
+const FileList: NextPage = () => {
+  return (
+    <Dashboardlayout headerTitle="Badri mohallah">
+      <h1>File List</h1>
+    </Dashboardlayout>
+  );
+};
+
+export default FileList;
