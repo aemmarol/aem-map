7524daa2e7a41d02e24484a59201842559c19736
alphanoobie <hussaingadi.hg@gmail.com>
2022-03-11 13:34:03 +0530

    fixed positioning according to the steps

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,5 @@
-import {NextPage} from "next";
-import {useEffect} from "react";
-import {Dashboardlayout} from "../layouts/dashboardLayout";
+import { NextPage } from "next";
+import { Dashboardlayout } from "../layouts/dashboardLayout";
 import styles from "../styles/map.module.scss";
 
 // create a div with height 642
@@ -15,40 +14,43 @@ const sectorImgArr = [
     left: 0,
     top: 0,
   },
+  {
+    src: "/map-images/najmi.png",
+    left: 340,
+    top: 0,
+  },
+  {
+    src: "/map-images/mohammedi.png",
+    left: 726,
+    top: 126,
+  },
+  {
+    src: "/map-images/hakimi.png",
+    left: 480,
+    top: 0,
+  },
+  {
+    src: "/map-images/vajihi.png",
+    left: 269,
+    top: 250,
+  },
 ];
 
 const Map: NextPage = () => {
   return (
     <Dashboardlayout headerTitle="Map View">
-      <img src="/map-images/view1_base.png" className={styles.areaImage} />
-      {sectorImgArr.map((val, index) => (
-        <img
-          src={val.src}
-          className={styles.areaImage}
-          style={{left: val.left, top: val.top}}
-        />
-      ))}
-      {/* <img src="/map-images/shujayi.png" className={styles.areaImage} /> */}
-      <img
-        src="/map-images/najmi.png"
-        className={styles.areaImage}
-        style={{left: "24.5%"}}
-      />
-      <img
-        src="/map-images/hakimi.png"
-        className={styles.areaImage}
-        style={{left: "33.5%"}}
-      />
-      <img
-        src="/map-images/mohammedi.png"
-        className={styles.areaImage}
-        style={{left: "49.5%", bottom: 0}}
-      />
-      <img
-        src="/map-images/vajihi.png"
-        className={styles.areaImage}
-        style={{left: "19.7%", top: "47%"}}
-      />
+      <div className={styles.mainWrapper}>
+        <div className={styles.relativeWrapper}>
+          <img src="/map-images/view1_base.png" className={styles.baseImage} />
+          {sectorImgArr.map((val, index) => (
+            <img
+              src={val.src}
+              className={styles.areaImage}
+              style={{ left: val.left, top: val.top }}
+            />
+          ))}
+        </div>
+      </div>
     </Dashboardlayout>
   );
 };
