9d4b551eb7c526242b61b495364621c8c4c9caa4
adnandedhawala <adnan.d@somaiya.edu>
2022-03-11 12:33:42 +0530

    format code

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,6 @@
-import { NextPage } from "next";
-import { useEffect } from "react";
-import { Dashboardlayout } from "../layouts/dashboardLayout";
+import {NextPage} from "next";
+import {useEffect} from "react";
+import {Dashboardlayout} from "../layouts/dashboardLayout";
 import styles from "../styles/map.module.scss";
 
 // create a div with height 642
@@ -9,45 +9,45 @@ import styles from "../styles/map.module.scss";
 // bg image css will be height 642 with auto
 //  dive will be position relative and center aligned to the screen
 
-
-const sectorImgArr = [{
-  src: "/map-images/shujayi.png",
-  left: 0,
-  top: 0,
-
-}]
+const sectorImgArr = [
+  {
+    src: "/map-images/shujayi.png",
+    left: 0,
+    top: 0,
+  },
+];
 
 const Map: NextPage = () => {
   return (
     <Dashboardlayout headerTitle="Map View">
       <img src="/map-images/view1_base.png" className={styles.areaImage} />
-      {
-        sectorImgArr.map((val, index) => <img
+      {sectorImgArr.map((val, index) => (
+        <img
           src={val.src}
           className={styles.areaImage}
-          style={{ left: val.left,top:val.top }}
-        />)
-      }
+          style={{left: val.left, top: val.top}}
+        />
+      ))}
       {/* <img src="/map-images/shujayi.png" className={styles.areaImage} /> */}
       <img
         src="/map-images/najmi.png"
         className={styles.areaImage}
-        style={{ left: "24.5%" }}
+        style={{left: "24.5%"}}
       />
       <img
         src="/map-images/hakimi.png"
         className={styles.areaImage}
-        style={{ left: "33.5%" }}
+        style={{left: "33.5%"}}
       />
       <img
         src="/map-images/mohammedi.png"
         className={styles.areaImage}
-        style={{ left: "49.5%", bottom: 0 }}
+        style={{left: "49.5%", bottom: 0}}
       />
       <img
         src="/map-images/vajihi.png"
         className={styles.areaImage}
-        style={{ left: "19.7%", top: "47%" }}
+        style={{left: "19.7%", top: "47%"}}
       />
     </Dashboardlayout>
   );
