32679053e08b8d7eb9aa87858f9acca86abdae38
alphanoobie <hussaingadi.hg@gmail.com>
2022-03-27 16:39:50 +0530

    added markers

--- components/maps/map1.tsx
+++ components/maps/map1.tsx
@@ -1,32 +1,39 @@
 import {FC, useEffect} from "react";
+import {ImLocation} from "react-icons/im";
 import styles from "../../styles/map.module.scss";
 import useWindowDimensions from "../../utils/windowDimensions";
+import {Popover} from "antd";
 
 const sectorImgArr = [
   {
     src: "/map-images/shujayi.png",
     left: 0,
     top: 0,
+    label: "Shujayi",
   },
   {
     src: "/map-images/najmi.png",
     left: 340,
     top: 0,
+    label: "Najmi",
   },
   {
     src: "/map-images/mohammedi.png",
     left: 726,
     top: 126,
+    label: "Mohammedi",
   },
   {
     src: "/map-images/hakimi.png",
     left: 480,
     top: 0,
+    label: "Hakimi",
   },
   {
     src: "/map-images/vajihi.png",
     left: 269,
     top: 250,
+    label: "Vajihi",
   },
 ];
 
@@ -41,12 +48,22 @@ export const Map1: FC = () => {
     <div className={styles.mainWrapper}>
       <div className={styles.relativeWrapper}>
         <img src="/map-images/view1_base.png" className={styles.baseImage} />
-        {sectorImgArr.map((val, index) => (
-          <img
-            src={val.src}
-            className={styles.areaImage}
-            style={{left: val.left, top: val.top}}
-          />
+        {sectorImgArr.map((val) => (
+          <div>
+            <img
+              src={val.src}
+              className={styles.areaImage}
+              style={{left: val.left, top: val.top}}
+            />
+            <span>
+              <Popover content={val.label}>
+                <ImLocation
+                  style={{left: val.left + 90, top: val.top + 90}}
+                  className={styles.areaImage}
+                />
+              </Popover>
+            </span>
+          </div>
         ))}
       </div>
     </div>
