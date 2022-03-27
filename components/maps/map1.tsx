a8ee99400f5ea0d807f95fa25468c4ae50e469f6
alphanoobie <hussaingadi.hg@gmail.com>
2022-03-27 17:10:07 +0530

    marker styling

--- components/maps/map1.tsx
+++ components/maps/map1.tsx
@@ -1,6 +1,6 @@
 import {FC, useEffect} from "react";
 import {ImLocation} from "react-icons/im";
-import styles from "../../styles/map.module.scss";
+import styles from "../../styles/components/maps/map1.module.scss";
 import useWindowDimensions from "../../utils/windowDimensions";
 import {Popover} from "antd";
 
@@ -9,30 +9,40 @@ const sectorImgArr = [
     src: "/map-images/shujayi.png",
     left: 0,
     top: 0,
+    markerLeft:100,
+    markerTop:300,
     label: "Shujayi",
   },
   {
     src: "/map-images/najmi.png",
     left: 340,
     top: 0,
+    markerLeft:460,
+    markerTop:300,
     label: "Najmi",
   },
   {
     src: "/map-images/mohammedi.png",
     left: 726,
     top: 126,
+    markerLeft:950,
+    markerTop:300,
     label: "Mohammedi",
   },
   {
     src: "/map-images/hakimi.png",
     left: 480,
     top: 0,
+    markerLeft:640,
+    markerTop:120,
     label: "Hakimi",
   },
   {
     src: "/map-images/vajihi.png",
     left: 269,
     top: 250,
+    markerLeft:340,
+    markerTop:300,
     label: "Vajihi",
   },
 ];
@@ -48,8 +58,8 @@ export const Map1: FC = () => {
     <div className={styles.mainWrapper}>
       <div className={styles.relativeWrapper}>
         <img src="/map-images/view1_base.png" className={styles.baseImage} />
-        {sectorImgArr.map((val) => (
-          <div>
+        {sectorImgArr.map((val,label) => (
+          <div key={label}>
             <img
               src={val.src}
               className={styles.areaImage}
@@ -58,8 +68,8 @@ export const Map1: FC = () => {
             <span>
               <Popover content={val.label}>
                 <ImLocation
-                  style={{left: val.left + 90, top: val.top + 90}}
-                  className={styles.areaImage}
+                  style={{left: val.markerLeft, top: val.markerTop}}
+                  className={styles.marker}
                 />
               </Popover>
             </span>
