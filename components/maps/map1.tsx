1dae93b2ed9bafe8eed102e90f571a1b4e30a69c
alphanoobie <hussaingadi.hg@gmail.com>
2022-04-04 23:26:15 +0530

    failed attempt of fetching priamry color

--- components/maps/map1.tsx
+++ components/maps/map1.tsx
@@ -1,48 +1,50 @@
-import {FC, useEffect} from "react";
+import {FC, useEffect, useState} from "react";
 import {ImLocation} from "react-icons/im";
 import styles from "../../styles/components/maps/map1.module.scss";
 import useWindowDimensions from "../../utils/windowDimensions";
 import {Popover} from "antd";
+import {getSectorData} from "../../pages/api/v1/db/sectorCrud";
+import {sectorData} from "../../types";
 
 const sectorImgArr = [
   {
     src: "/map-images/shujayi.png",
     left: 0,
     top: 0,
-    markerLeft:100,
-    markerTop:300,
+    markerLeft: 100,
+    markerTop: 300,
     label: "Shujayi",
   },
   {
     src: "/map-images/najmi.png",
     left: 340,
     top: 0,
-    markerLeft:460,
-    markerTop:300,
+    markerLeft: 460,
+    markerTop: 300,
     label: "Najmi",
   },
   {
     src: "/map-images/mohammedi.png",
     left: 726,
     top: 126,
-    markerLeft:950,
-    markerTop:300,
+    markerLeft: 950,
+    markerTop: 300,
     label: "Mohammedi",
   },
   {
     src: "/map-images/hakimi.png",
     left: 480,
     top: 0,
-    markerLeft:640,
-    markerTop:120,
+    markerLeft: 640,
+    markerTop: 120,
     label: "Hakimi",
   },
   {
     src: "/map-images/vajihi.png",
     left: 269,
     top: 250,
-    markerLeft:340,
-    markerTop:300,
+    markerLeft: 340,
+    markerTop: 300,
     label: "Vajihi",
   },
 ];
@@ -50,15 +52,29 @@ const sectorImgArr = [
 export const Map1: FC = () => {
   const {height: windowHeight, width: windowWidth} = useWindowDimensions();
 
+  function sectorColor(sectorName: string) {
+    const [primaryColor, setPrimaryColor] = useState("");
+    useEffect(() => {
+      try {
+        getSectorData(sectorName).then((data) => {
+          setPrimaryColor(data.primary_color);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  }
+
   useEffect(() => {
     console.log("dimensions", windowHeight, windowWidth);
   }, [windowHeight, windowWidth]);
 
   return (
     <div className={styles.mainWrapper}>
+      <div>{getSectorData("Vajihi")}</div>
       <div className={styles.relativeWrapper}>
         <img src="/map-images/view1_base.png" className={styles.baseImage} />
-        {sectorImgArr.map((val,label) => (
+        {sectorImgArr.map((val, label) => (
           <div key={label}>
             <img
               src={val.src}
