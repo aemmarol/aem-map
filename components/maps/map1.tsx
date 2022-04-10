a8593ccad738254bc62c4225ec1c498607aa5649
alphanoobie <hussaingadi.hg@gmail.com>
2022-04-10 13:57:31 +0530

    added colors to markers

--- components/maps/map1.tsx
+++ components/maps/map1.tsx
@@ -3,8 +3,9 @@ import {ImLocation} from "react-icons/im";
 import styles from "../../styles/components/maps/map1.module.scss";
 import useWindowDimensions from "../../utils/windowDimensions";
 import {Popover} from "antd";
-import {getSectorData} from "../../pages/api/v1/db/sectorCrud";
+import {getSectorList} from "../../pages/api/v1/db/sectorCrud";
 import {sectorData} from "../../types";
+import {get, find, findIndex} from "lodash";
 
 const sectorImgArr = [
   {
@@ -52,30 +53,36 @@ const sectorImgArr = [
 export const Map1: FC = () => {
   const {height: windowHeight, width: windowWidth} = useWindowDimensions();
 
-  function sectorColor(sectorName: string) {
-    const [primaryColor, setPrimaryColor] = useState("");
-    useEffect(() => {
-      try {
-        getSectorData(sectorName).then((data) => {
-          setPrimaryColor(data.primary_color);
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  }
+  const [mohallahlist, setMohallahlist] = useState<sectorData[]>([]);
+
+  useEffect(() => {
+    setList();
+  }, []);
+
+  const setList = async () => {
+    const listData: sectorData[] = await getSectorList();
+    setMohallahlist(listData);
+  };
 
   useEffect(() => {
     console.log("dimensions", windowHeight, windowWidth);
   }, [windowHeight, windowWidth]);
 
+  const getPrimaryColor = (sectorName: string) => {
+    const sectorNameUpper = sectorName.toUpperCase();
+    const index = findIndex(mohallahlist, function (o) {
+      return o.name == sectorNameUpper;
+    });
+    return get(mohallahlist, [index, "primary_color"]);
+  };
+
   return (
     <div className={styles.mainWrapper}>
-      <div>{getSectorData("Vajihi")}</div>
       <div className={styles.relativeWrapper}>
         <img src="/map-images/view1_base.png" className={styles.baseImage} />
         {sectorImgArr.map((val, label) => (
           <div key={label}>
+            {console.log(getPrimaryColor(val.label))}
             <img
               src={val.src}
               className={styles.areaImage}
@@ -84,7 +91,11 @@ export const Map1: FC = () => {
             <span>
               <Popover content={val.label}>
                 <ImLocation
-                  style={{left: val.markerLeft, top: val.markerTop}}
+                  style={{
+                    left: val.markerLeft,
+                    top: val.markerTop,
+                    color: getPrimaryColor(val.label),
+                  }}
                   className={styles.marker}
                 />
               </Popover>
