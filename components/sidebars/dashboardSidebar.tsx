13fb76a1a38937b866d1969bde765591244bfff9
adnandedhawala <adnan.d@somaiya.edu>
2022-05-01 17:24:41 +0530

    solved routing error

--- components/sidebars/dashboardSidebar.tsx
+++ components/sidebars/dashboardSidebar.tsx
@@ -1,13 +1,30 @@
-import {Divider, Drawer, Image, Menu} from "antd";
-import {useRouter} from "next/router";
-import {FC} from "react";
+import { Divider, Drawer, Image, Menu } from "antd";
+import { useRouter } from "next/router";
+import { FC } from "react";
+import { verifyUser } from "../../pages/api/v1/authentication";
+import { getSubSectorDataByName } from "../../pages/api/v1/db/subSectorCrud";
 import styles from "../../styles/components/sidebars/dashboardSidebar.module.scss";
+import { authUser } from "../../types";
 
 export const DashboardSidebar: FC<{
   visible: boolean;
   handleClose: () => any;
-}> = ({visible, handleClose}) => {
+}> = ({ visible, handleClose }) => {
   const router = useRouter();
+
+  const handleMohallahRouting = async () => {
+    if (typeof verifyUser() !== "string") {
+      const { userRole, assignedArea } = verifyUser() as authUser;
+      if (userRole.includes("Admin")) {
+        router.push("/mohallah")
+      } else if (userRole.includes("Masool") || userRole.includes("Masoola")) {
+        router.push("/mohallah/" + assignedArea[0])
+      } else if (userRole.includes("Musaid") || userRole.includes("Musaida")) {
+        const subsectorDetails = await getSubSectorDataByName(assignedArea[0])
+        router.push("/mohallah/" + subsectorDetails.sector.name + "/" + assignedArea[0])
+      }
+    }
+  }
 
   return (
     <Drawer
@@ -29,7 +46,7 @@ export const DashboardSidebar: FC<{
       </div>
       <Divider />
       <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1" onClick={() => router.push("/mohallah")}>
+        <Menu.Item key="1" onClick={handleMohallahRouting}>
           Mohallah
         </Menu.Item>
         <Menu.Item key="4">Escalations</Menu.Item>
