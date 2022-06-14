dc053bf990556f6ef40c7f2a3f5fbe73c6ce012f
adnandedhawala <34675754+adnandedhawala@users.noreply.github.com>
2022-06-14 08:27:57 +0530

    Update index.ts

--- pages/api/v2/db/escalation/index.ts
+++ pages/api/v2/db/escalation/index.ts
@@ -1,33 +1,36 @@
 import nextConnect from "next-connect";
-import { escalationCollectionName } from "../../../../../firebase/dbCollectionNames";
-import getAuthHandler, { NextApiRequestExtended } from "../../../../../mongodb/authHandler";
+import {escalationCollectionName} from "../../../../../firebase/dbCollectionNames";
+import getAuthHandler, {
+  NextApiRequestExtended,
+} from "../../../../../mongodb/authHandler";
 import middleware from "../../../../../mongodb/database";
-import { userRoles } from "../../../../../types";
+import {userRoles} from "../../../../../types";
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
-export default getAuthHandler().get(async (req: NextApiRequestExtended, res) => {
-  const { userData } = req;
-  if (userData.userRole.includes(userRoles.Admin)) {
-    const doc = await req.db
-      .collection(escalationCollectionName)
-      .find()
-      .toArray();
-    res.json(doc);
-  } else {
-    res.status(401).json({ msg: "user access denied!" });
+export default getAuthHandler().get(
+  async (req: NextApiRequestExtended, res) => {
+    const {userData} = req;
+    if (userData.userRole.includes(userRoles.Admin)) {
+      const doc = await req.db
+        .collection(escalationCollectionName)
+        .find()
+        .toArray();
+      res.json(doc);
+    } else {
+      res.status(401).json({msg: "user access denied!"});
+    }
   }
-})
-
+);
 
 // handler.get(async (req: any, res: any) => {
-  // const doc = await req.db
-  //   .collection(escalationCollectionName)
-  //   .find()
-  //   .toArray();
-  // res.json(doc);
+// const doc = await req.db
+//   .collection(escalationCollectionName)
+//   .find()
+//   .toArray();
+// res.json(doc);
 // });
 
 // handler.post(async (req: any, res: any) => {
