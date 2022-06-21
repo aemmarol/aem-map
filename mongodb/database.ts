3346af0cae7286f022b451d5bea4c2334d4023bb
adnandedhawala <34675754+adnandedhawala@users.noreply.github.com>
2022-06-22 02:35:47 +0530

    added mongo env variables

--- mongodb/database.ts
+++ mongodb/database.ts
@@ -1,18 +1,21 @@
 import {MongoClient} from "mongodb";
 import nextConnect from "next-connect";
-const connectionString = "mongodb://43.204.75.157:27017";
+const connectionString =
+  "mongodb+srv://" +
+  process.env.NEXT_PUBLIC_MONGODB_USERNAME +
+  ":" +
+  process.env.NEXT_PUBLIC_MONGODB_PASSWORD +
+  "@" +
+  process.env.NEXT_PUBLIC_MONGODB_HOST +
+  "/?retryWrites=true&w=majority";
+
 const client = new MongoClient(connectionString);
 
 async function database(req: any, res: any, next: any) {
-  await client.connect();
-  req.dbClient = client;
-  req.db = client.db("aem-prod");
+  const newCon = await client.connect();
+  req.db = newCon.db(process.env.NEXT_PUBLIC_MONGODB_DBNAME);
   return next();
 }
-
-// const isConnected = ()=>{
-//     return !!client && !!client.db && client.topology.isConnected()
-// }
 
 const middleware = nextConnect();
 
