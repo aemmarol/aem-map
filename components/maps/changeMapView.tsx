82b678ad244db015224c53190a3c23454eb2bd45
adnandedhawala <34675754+adnandedhawala@users.noreply.github.com>
2022-06-25 11:44:14 +0530

    comment map code

--- components/maps/changeMapView.tsx
+++ components/maps/changeMapView.tsx
@@ -1,18 +1,19 @@
-import {LatLngBoundsExpression, LatLngExpression} from "leaflet";
+// import {LatLngBoundsExpression, LatLngExpression} from "leaflet";
 import {FC} from "react";
-import {useMap} from "react-leaflet";
+// import {useMap} from "react-leaflet";
 import {sectorData} from "../../types";
-const ChangeMapView: FC<{sector: sectorData}> = ({sector}) => {
-  const map = useMap();
-  if (!!sector && !!sector.latlng) {
-    map.setView(sector.latlng as LatLngExpression);
-    map.fitBounds(
-      sector.bounds?.map((val: any) => [
-        val.lat,
-        val.lang,
-      ]) as LatLngBoundsExpression
-    );
-  }
+// const ChangeMapView: FC<{sector: sectorData}> = ({sector}) => {
+const ChangeMapView: FC<{sector: sectorData}> = () => {
+  // const map = useMap();
+  // if (!!sector && !!sector.latlng) {
+  //   map.setView(sector.latlng as LatLngExpression);
+  //   map.fitBounds(
+  //     sector.bounds?.map((val: any) => [
+  //       val.lat,
+  //       val.lang,
+  //     ]) as LatLngBoundsExpression
+  //   );
+  // }
 
   return null;
 };
