2ea9e86a1c917c5f80436477f7a9a4821739a2a6
adnandedhawala <34675754+adnandedhawala@users.noreply.github.com>
2022-07-11 12:52:56 +0530

    Update index.ts

--- pages/api/v2/db/member/index.ts
+++ pages/api/v2/db/member/index.ts
@@ -1,25 +1,20 @@
 import getAuthHandler, {
   NextApiRequestExtended,
 } from "../../../../../mongodb/authHandler";
-import {memberCollectionName} from "../../../../../mongodb/dbCollectionNames";
-import {userRoles} from "../../../../../types";
+import { memberCollectionName } from "../../../../../mongodb/dbCollectionNames";
 
 export default getAuthHandler().get(
   async (req: NextApiRequestExtended, res) => {
-    const {userData} = req;
-    const {itsId} = req.query;
-    if (userData.userRole.includes(userRoles.Admin)) {
-      if (!itsId) {
-        res.status(400).json({msg: "invalid request!"});
-      } else {
-        const qValue: string = itsId as string;
-        const doc = await req.db
-          .collection(memberCollectionName)
-          .findOne({_id: qValue});
-        res.json(doc);
-      }
+    const { itsId } = req.query;
+    if (!itsId) {
+      res.status(400).json({ msg: "invalid request!" });
     } else {
-      res.status(401).json({msg: "user access denied!"});
+      const qValue: string = itsId as string;
+      const doc = await req.db
+        .collection(memberCollectionName)
+        .findOne({ _id: qValue });
+      res.json(doc);
     }
+
   }
 );
