cf4b0ad273430fff343585ee15695fd952143cd9
adnandedhawala <34675754+adnandedhawala@users.noreply.github.com>
2022-07-11 12:53:04 +0530

    Update index.ts

--- pages/api/v2/db/member/index.ts
+++ pages/api/v2/db/member/index.ts
@@ -1,20 +1,19 @@
 import getAuthHandler, {
   NextApiRequestExtended,
 } from "../../../../../mongodb/authHandler";
-import { memberCollectionName } from "../../../../../mongodb/dbCollectionNames";
+import {memberCollectionName} from "../../../../../mongodb/dbCollectionNames";
 
 export default getAuthHandler().get(
   async (req: NextApiRequestExtended, res) => {
-    const { itsId } = req.query;
+    const {itsId} = req.query;
     if (!itsId) {
-      res.status(400).json({ msg: "invalid request!" });
+      res.status(400).json({msg: "invalid request!"});
     } else {
       const qValue: string = itsId as string;
       const doc = await req.db
         .collection(memberCollectionName)
-        .findOne({ _id: qValue });
+        .findOne({_id: qValue});
       res.json(doc);
     }
-
   }
 );
