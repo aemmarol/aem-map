a3cea5bfec78906bbe60bb6744c77ffa54b663bd
adnandedhawala <ddedhawala@gmail.com>
2025-02-23 18:31:55 +0530

    fixed build

--- pages/escalations/index.tsx
+++ pages/escalations/index.tsx
@@ -1,4 +1,4 @@
-import {Button, message, Select, Tooltip} from "antd";
+import {Button, message, Select} from "antd";
 import {NextPage} from "next";
 import {useRouter} from "next/router";
 import {Dashboardlayout} from "../../layouts/dashboardLayout";
@@ -19,9 +19,6 @@ import {EscalationList} from "../../components/custom/escalations/escalationList
 import {filterTypes} from "../../types/escalation";
 import {StatsCard} from "../../components/cards/statsCard";
 import useWindowDimensions from "../../utils/windowDimensions";
-import {DownloadOutlined} from "@ant-design/icons";
-import {CSVLink} from "react-csv";
-import {getauthToken} from "../../utils";
 import {getSectorList} from "../api/v2/services/sector";
 import {getUmoorListWithCoordinators} from "../api/v2/services/umoor";
 import {useEscalationContext} from "../../context/EscalationContext";
@@ -32,9 +29,6 @@ import {
   getSectorStats,
   getUmoorStats,
 } from "../api/v2/services/escalation";
-import {API} from "../../utils/api";
-import {handleResponse} from "../../utils/handleResponse";
-import moment from "moment";
 
 const EscalationDashboard: NextPage = () => {
   const router = useRouter();
@@ -48,7 +42,6 @@ const EscalationDashboard: NextPage = () => {
     selectedfilterItems,
     setEscalationList,
     setSelectedFilterItems,
-    escalationList,
   } = useEscalationContext();
   const {width} = useWindowDimensions();
 
@@ -57,7 +50,6 @@ const EscalationDashboard: NextPage = () => {
 
   const [statCardList, setStatCardList] = useState<any>([]);
   const [umoorList, setUmoorList] = useState<umoorData[]>([]);
-  const [userList, setUserList] = useState<umoorData[]>([]);
 
   useEffect(() => {
     if (typeof verifyUser() !== "string") {
@@ -90,13 +82,13 @@ const EscalationDashboard: NextPage = () => {
     const umoors: umoorData[] = await getUmoorListWithCoordinators();
     setUmoorList(umoors);
 
-    const users = await await fetch(API.userList, {
-      method: "GET",
-      headers: {...getauthToken()},
-    })
-      .then(handleResponse)
-      .catch((error) => message.error(error.message));
-    setUserList(users);
+    // const users = await await fetch(API.userList, {
+    //   method: "GET",
+    //   headers: {...getauthToken()},
+    // })
+    //   .then(handleResponse)
+    //   .catch((error) => message.error(error.message));
+    // setUserList(users);
 
     if (user.userRole.includes(userRoles.Admin)) {
       await getSectorList((data: sectorData[]) => {
@@ -339,111 +331,111 @@ const EscalationDashboard: NextPage = () => {
     setShowEscalationModal(true);
   };
 
-  const getEscalationDownloadData: any = () => {
-    const tempArr: any = escalationList
-      .sort((a: any, b: any) => {
-        const aEsc = Number(a.escalation_id.split("-")[1]);
-        const bEsc = Number(b.escalation_id.split("-")[1]);
-        return aEsc - bEsc;
-      })
-      .map((value: any) => {
-        const masool: any = userList.filter(
-          (user: any) =>
-            user.assignedArea.includes(
-              value.file_details.sub_sector.sector.name
-            ) && user.userRole.includes(userRoles.Masool)
-        );
-        const masoola: any = userList.filter(
-          (user: any) =>
-            user.assignedArea.includes(
-              value.file_details.sub_sector.sector.name
-            ) && user.userRole.includes(userRoles.Masoola)
-        );
-        const musaid: any = userList.filter(
-          (user: any) =>
-            user.assignedArea.includes(value.file_details.sub_sector.name) &&
-            user.userRole.includes(userRoles.Musaid)
-        );
-        const musaida: any = userList.filter(
-          (user: any) =>
-            user.assignedArea.includes(value.file_details.sub_sector.name) &&
-            user.userRole.includes(userRoles.Musaida)
-        );
-        return {
-          ...value,
-          ...value.issueRaisedFor,
-          ...value.file_details,
-          ...value.type,
-          sector: value.file_details.sub_sector.sector.name,
-          sub_sector: value.file_details.sub_sector.name,
-          cb_name: value.created_by.name,
-          cb_its: value.created_by.its_number,
-          cb_contact: value.created_by.contact_number,
-          cb_role: value.created_by.userRole,
-          masool_name: masool[0] && masool[0].name ? masool[0].name : "-",
-          masool_contact:
-            masool[0] && masool[0].contact ? masool[0].contact : "-",
-          masool_its: masool[0] && masool[0].itsId ? masool[0].itsId : "-",
-          masoola_name: masoola[0] && masoola[0].name ? masoola[0].name : "-",
-          masoola_contact:
-            masoola[0] && masoola[0].contact ? masoola[0].contact : "-",
-          masoola_its: masoola[0] && masoola[0].itsId ? masoola[0].itsId : "-",
-          musaid_name: musaid[0] && musaid[0].name ? musaid[0].name : "-",
-          musaid_contact:
-            musaid[0] && musaid[0].contact ? musaid[0].contact : "-",
-          musaid_its: musaid[0] && musaid[0].itsId ? musaid[0].itsId : "-",
-          musaida_name: musaida[0] && musaida[0].name ? musaida[0].name : "-",
-          musaida_contact:
-            musaida[0] && musaida[0].contact ? musaida[0].contact : "-",
-          musaida_its: musaida[0] && musaida[0].itsId ? musaida[0].itsId : "-",
-          created_at: moment(value.created_at, "DD-MM-YYYY hh:mm:ss").format(
-            "DD-MM-YYYY"
-          ),
-        };
-      });
+  // const getEscalationDownloadData: any = () => {
+  //   const tempArr: any = escalationList
+  //     .sort((a: any, b: any) => {
+  //       const aEsc = Number(a.escalation_id.split("-")[1]);
+  //       const bEsc = Number(b.escalation_id.split("-")[1]);
+  //       return aEsc - bEsc;
+  //     })
+  //     .map((value: any) => {
+  //       const masool: any = userList.filter(
+  //         (user: any) =>
+  //           user.assignedArea.includes(
+  //             value.file_details.sub_sector.sector.name
+  //           ) && user.userRole.includes(userRoles.Masool)
+  //       );
+  //       const masoola: any = userList.filter(
+  //         (user: any) =>
+  //           user.assignedArea.includes(
+  //             value.file_details.sub_sector.sector.name
+  //           ) && user.userRole.includes(userRoles.Masoola)
+  //       );
+  //       const musaid: any = userList.filter(
+  //         (user: any) =>
+  //           user.assignedArea.includes(value.file_details.sub_sector.name) &&
+  //           user.userRole.includes(userRoles.Musaid)
+  //       );
+  //       const musaida: any = userList.filter(
+  //         (user: any) =>
+  //           user.assignedArea.includes(value.file_details.sub_sector.name) &&
+  //           user.userRole.includes(userRoles.Musaida)
+  //       );
+  //       return {
+  //         ...value,
+  //         ...value.issueRaisedFor,
+  //         ...value.file_details,
+  //         ...value.type,
+  //         sector: value.file_details.sub_sector.sector.name,
+  //         sub_sector: value.file_details.sub_sector.name,
+  //         cb_name: value.created_by.name,
+  //         cb_its: value.created_by.its_number,
+  //         cb_contact: value.created_by.contact_number,
+  //         cb_role: value.created_by.userRole,
+  //         masool_name: masool[0] && masool[0].name ? masool[0].name : "-",
+  //         masool_contact:
+  //           masool[0] && masool[0].contact ? masool[0].contact : "-",
+  //         masool_its: masool[0] && masool[0].itsId ? masool[0].itsId : "-",
+  //         masoola_name: masoola[0] && masoola[0].name ? masoola[0].name : "-",
+  //         masoola_contact:
+  //           masoola[0] && masoola[0].contact ? masoola[0].contact : "-",
+  //         masoola_its: masoola[0] && masoola[0].itsId ? masoola[0].itsId : "-",
+  //         musaid_name: musaid[0] && musaid[0].name ? musaid[0].name : "-",
+  //         musaid_contact:
+  //           musaid[0] && musaid[0].contact ? musaid[0].contact : "-",
+  //         musaid_its: musaid[0] && musaid[0].itsId ? musaid[0].itsId : "-",
+  //         musaida_name: musaida[0] && musaida[0].name ? musaida[0].name : "-",
+  //         musaida_contact:
+  //           musaida[0] && musaida[0].contact ? musaida[0].contact : "-",
+  //         musaida_its: musaida[0] && musaida[0].itsId ? musaida[0].itsId : "-",
+  //         created_at: moment(value.created_at, "DD-MM-YYYY hh:mm:ss").format(
+  //           "DD-MM-YYYY"
+  //         ),
+  //       };
+  //     });
 
-    return tempArr;
-  };
+  //   return tempArr;
+  // };
 
-  const getEscalationDownloadDataHeaders = () => {
-    const columns = [
-      {key: "escalation_id", label: "Escalation Id"},
-      {key: "ITS", label: "ITS"},
-      {key: "name", label: "Issue raised for"},
-      {key: "contact", label: "Issue raised for contact"},
-      {key: "issue", label: "Issue"},
-      {key: "status", label: "Status"},
-      {key: "created_at", label: "Issue Raised On"},
-      {key: "label", label: "Umoor"},
-      {
-        key: "tanzeem_file_no",
-        label: "File number",
-      },
-      {key: "hof_its", label: "HOF ITS"},
-      {key: "hof_name", label: "HOF Name"},
-      {key: "hof_contact", label: "HOF contact"},
-      {key: "sector", label: "Sector"},
-      {key: "sub_sector", label: "Sub Sector"},
-      {key: "address", label: "Address"},
-      {key: "cb_name", label: "Created By"},
-      {key: "cb_its", label: "Created By ITS"},
-      {key: "cb_contact", label: "Created By Contact"},
-      {key: "cb_role", label: "Created By userRole"},
-      {key: "masool_name", label: "Masool Name"},
-      {key: "masool_contact", label: "Masool Contact"},
-      {key: "masool_its", label: "Masool ITS"},
-      {key: "masoola_name", label: "Masoola Name"},
-      {key: "masoola_contact", label: "Masoola Contact"},
-      {key: "masoola_its", label: "Masoola ITS"},
-      {key: "musaid_name", label: "Musaid Name"},
-      {key: "musaid_contact", label: "Musaid Contact"},
-      {key: "musaid_its", label: "Musaid ITS"},
-      {key: "musaida_name", label: "Musaida Name"},
-      {key: "musaida_contact", label: "Musaida Contact"},
-      {key: "musaida_its", label: "Musaida ITS"},
-    ];
-    return columns;
-  };
+  // const getEscalationDownloadDataHeaders = () => {
+  //   const columns = [
+  //     {key: "escalation_id", label: "Escalation Id"},
+  //     {key: "ITS", label: "ITS"},
+  //     {key: "name", label: "Issue raised for"},
+  //     {key: "contact", label: "Issue raised for contact"},
+  //     {key: "issue", label: "Issue"},
+  //     {key: "status", label: "Status"},
+  //     {key: "created_at", label: "Issue Raised On"},
+  //     {key: "label", label: "Umoor"},
+  //     {
+  //       key: "tanzeem_file_no",
+  //       label: "File number",
+  //     },
+  //     {key: "hof_its", label: "HOF ITS"},
+  //     {key: "hof_name", label: "HOF Name"},
+  //     {key: "hof_contact", label: "HOF contact"},
+  //     {key: "sector", label: "Sector"},
+  //     {key: "sub_sector", label: "Sub Sector"},
+  //     {key: "address", label: "Address"},
+  //     {key: "cb_name", label: "Created By"},
+  //     {key: "cb_its", label: "Created By ITS"},
+  //     {key: "cb_contact", label: "Created By Contact"},
+  //     {key: "cb_role", label: "Created By userRole"},
+  //     {key: "masool_name", label: "Masool Name"},
+  //     {key: "masool_contact", label: "Masool Contact"},
+  //     {key: "masool_its", label: "Masool ITS"},
+  //     {key: "masoola_name", label: "Masoola Name"},
+  //     {key: "masoola_contact", label: "Masoola Contact"},
+  //     {key: "masoola_its", label: "Masoola ITS"},
+  //     {key: "musaid_name", label: "Musaid Name"},
+  //     {key: "musaid_contact", label: "Musaid Contact"},
+  //     {key: "musaid_its", label: "Musaid ITS"},
+  //     {key: "musaida_name", label: "Musaida Name"},
+  //     {key: "musaida_contact", label: "Musaida Contact"},
+  //     {key: "musaida_its", label: "Musaida ITS"},
+  //   ];
+  //   return columns;
+  // };
 
   const successCallBack = async () => {
     await setupFiltersAndData();
@@ -512,7 +504,7 @@ const EscalationDashboard: NextPage = () => {
           >
             Raise Escalation
           </Button>
-          <Tooltip title="Download Escalationdata">
+          {/* <Tooltip title="Download Escalationdata">
             <CSVLink
               filename={"escalations.csv"}
               data={getEscalationDownloadData() || []}
@@ -521,7 +513,7 @@ const EscalationDashboard: NextPage = () => {
             >
               <DownloadOutlined style={{fontSize: 25}} />
             </CSVLink>
-          </Tooltip>
+          </Tooltip> */}
         </div>
       </div>
 
